59011153e88d4f75ca75dc5a9e7152786c76f51c
Juan Ignacio Pumarino <jipumarino@gmail.com>
2012-01-13 15:21:25 -0300

    Sólo una request cuando ya se tiene login

--- readability-kindle.rb
+++ readability-kindle.rb
@@ -6,11 +6,12 @@ require 'json'
 require 'logger'
 
 @logger = Logger.new('readability-kindle.log')
-
-@logger.info 'Parsing config file'
 @config = JSON.parse(File.read(File.join(File.dirname(__FILE__), 'config.json')))
-
 @agent = Mechanize.new
+
+def readability_url
+  "http://www.readability.com/#{@config['readability']['username']}/"
+end
 
 def deliver_article(id)
   @logger.info "Delivering article #{id}"
@@ -18,37 +19,33 @@ def deliver_article(id)
 end
 
 def archive_article(id)
-  @logger.info "Archiving article #{id}"
   @agent.get "http://www.readability.com/articles/#{id}/ajax/archive"
 end
 
 def process_articles
   @logger.info "Processing articles"
-  doc = Nokogiri::HTML(@agent.get("http://www.readability.com/#{@config['readability']['username']}/").body)
+  response = @agent.get readability_url
+  return false if response.forms.size > 1
+
+  doc = Nokogiri::HTML(response.body)
 
   articles_ids = doc.css('#rdb-reading-list a.list-article-title').map do |a|
     a['href'].match(/[^\/]+$/).to_s
   end
 
-  if articles_ids.empty?
-    @logger.info "Nothing to deliver"
-  end
+  @logger.info "Nothing to deliver" if articles_ids.empty?
 
   articles_ids.each do |article_id|
     deliver_article article_id
     archive_article article_id
   end
-end
 
-def logged_in?
-  @logger.info "Checking login"
-  response = @agent.get "http://www.readability.com/#{@config['readability']['username']}/"
-  response.forms.size == 1
+  return true
 end
 
 def try_to_login
   @logger.info "Trying to login"
-  login_page = @agent.get "http://www.readability.com/#{@config['readability']['username']}/"
+  login_page = @agent.get readability_url
   login_page.form.username = @config["readability"]["username"]
   login_page.form.password = @config["readability"]["password"]
   login_page.form.submit
@@ -57,12 +54,7 @@ end
 def main_loop
   @logger.info "Sleeping"
   sleep 60
-  if logged_in?
-    process_articles
-  else
-    try_to_login
-    process_articles if logged_in?
-  end
+  try_to_login if not process_articles
 end
 
 main_loop while true
